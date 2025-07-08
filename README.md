6e00556a4d491542a7c4a1acc1fa2bd2f4038e68
twonfi <git@twonum.org>
2025-07-08 12:26:42 -0700

    init
    so I just realized I can't submit this to boba drops because I live in canada...

---
+++ README.md
@@ -0,0 +1,13 @@
+# hub.twonum: My link hub (link-in-bio) tool
+[![try it on | hub.twonum.org](https://img.shields.io/badge/try_it_on-hub.twonum.org-%23222)](https://hub.twonum.org)
+![license | MIT/Expat](https://img.shields.io/badge/license-MIT%2FExpat-blue)
+
+This serves as the central link hub (a.k.a. "link-in-bio tool") similar to Linktree.
+
+Creating the link hub myself has the advantage of more privacy controls as well as Linktree not nagging you into signing
+up for their proprietary service.
+
+***
+
+## License (MIT/Expat)
+Copyright 2025 twonum. Licensed under the MIT/Expat license. See `LICENSE`.
---
+++ dialogs.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const DIALOG_BUTTONS = new Map(Object.entries({
+  'button-email': 'dialog-email',
+  'button-discord': 'dialog-discord',
+  'button-xbox': 'dialog-xbox',
+}));
+
+const CLOSE_BUTTON_TEMPLATE = document.getElementById("close-button-template").innerHTML;
+
+for (const BUTTON of DIALOG_BUTTONS) {
+    document.getElementById(BUTTON[0]).addEventListener('click', function() {
+    const DIALOG = document.getElementById(BUTTON[1]);
+    DIALOG.insertAdjacentHTML('beforeend', CLOSE_BUTTON_TEMPLATE);
+    const BUTTON_ELEMENT = DIALOG.lastElementChild;
+    BUTTON_ELEMENT.id = "close-button";
+    DIALOG.showModal();
+
+    DIALOG.addEventListener('close', function() {
+      try {
+        document.getElementById("close-button").remove();
+      } catch {}
+    });
+
+    BUTTON_ELEMENT.addEventListener('click', function() {
+      DIALOG.close();
+    });
+  });
+}
